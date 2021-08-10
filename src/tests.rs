20fc8aad4aa646e37e0989efb76e08f2155db9bb
Benjamin Tsui <benjamintsui@Benjamins-MacBook-Pro.local>
2021-08-09 18:07:29 -0600

    bubble sort

---
+++ src/tests.rs
@@ -0,0 +1,17 @@
+use crate::bubble_sort::bubble_sort;
+
+#[test]
+fn it_works() {
+    assert_eq!(2 + 2, 4);
+}
+
+
+
+
+#[test]
+fn test_bubble_sort() {
+
+    let mut v = vec![4, 6, 8, 11, 13, 3, 1];
+    bubble_sort(&mut v);
+    assert_eq!(v, vec![1,3,4,6,8,11,13]);
+}
